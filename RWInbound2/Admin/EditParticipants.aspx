a8070d8aa9e8ac325f11e7bf8d6b8f399582f840
amiscall <amiscall@istonish.com>
2016-10-12 17:23:29 -0600

    Started EditParticipants

---
+++ RWInbound2/Admin/EditParticipants.aspx
@@ -0,0 +1,61 @@
+﻿<%@ Page Title="Edit Participants" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="EditParticipants.aspx.cs" Inherits="RWInbound2.Admin.EditParticipants" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="HeaderContent" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="MainContent" runat="server">
+    <div>
+        <hgroup>
+            <h3><%: Page.Title %></h3>
+        </hgroup>
+
+        <div class="label-placement">
+            <asp:Label ID="ErrorLabel" CssClass="label-error" runat="server" />               
+        </div>
+        <div class="label-placement">
+            <asp:Label ID="SuccessLabel" CssClass="label-success" runat="server" />
+        </div>
+        <br />            
+    </div>
+    <div>
+        <asp:UpdatePanel ID="UpdatePanel1" runat="server">
+            <ContentTemplate>
+                Search By Organization:
+                <asp:TextBox ID="organizationSearch" 
+                    AutoPostBack="true"
+                    runat="server"></asp:TextBox>
+                <asp:Button ID="btnSearch" runat="server" Text="Select" Height="31px" OnClick="btnSearch_Click" CausesValidation="False" CssClass="adminButton" />
+                <asp:Button ID="btnSearchRefresh" runat="server" Text="Reset Search" Height="31px" OnClick="btnSearchRefresh_Click" CausesValidation="False" CssClass="adminButton"/>
+                    <ajaxToolkit:AutoCompleteExtender 
+                        ID="tbSearch_AutoCompleteExtender" 
+                        runat="server" 
+                        BehaviorID="tbSearch_AutoCompleteExtender" 
+                        DelimiterCharacters=""  
+                        ServiceMethod="SearchForOrganization"             
+                        TargetControlID="organizationSearch"
+                        MinimumPrefixLength="2"
+                        CompletionInterval="100" 
+                        EnableCaching="true" 
+                        CompletionSetCount="10"
+                        UseContextKey="True">
+                    </ajaxToolkit:AutoCompleteExtender> 
+            </ContentTemplate>
+        </asp:UpdatePanel>
+    </div>
+    <asp:FormView ID="ExpWaterFormView" runat="server" 
+        DataKeyNames="ID"
+        ItemType="RWInbound2.tblParticipant" 
+        SelectMethod="GetParticipants"
+        UpdateMethod="UpdateParticipants"
+        DefaultMode="Edit"
+        AllowPaging="true"> 
+        <EditItemTemplate> 
+            <asp:Button ID="UpdateButton" runat="server" Text="Update" CommandName="Update" CssClass="adminButton" />
+            <button class="adminButton" type="reset" value="Reset">Reset</button>
+            <table>
+                <tr>                    
+                    <label>Last Name:</label> 
+                <asp:TextBox ID="TextBoxLastName" runat="server" Text='<%# Bind("LastName") %>'></asp:TextBox> 
+                </tr>
+            </table>
+        </EditItemTemplate> 
+    </asp:FormView>
+</asp:Content>
---
+++ RWInbound2/Admin/EditParticipants.aspx.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Linq;
+using System.Web;
+using System.Web.UI.WebControls;
+using System.Web.ModelBinding;
+using System.Collections.Generic;
+using System.Text;
+using System.Data.Entity.Validation;
+using System.Reflection;
+
+namespace RWInbound2.Admin
+{
+    public partial class EditParticipants : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                SetMessages();
+                // Validate initially to force asterisks
+                // to appear before the first roundtrip.
+                Validate();
+            }
+        }
+
+        private void SetMessages(string type = "", string message = "")
+        {
+            switch (type)
+            {
+                case "Success":
+                    ErrorLabel.Text = "";
+                    SuccessLabel.Text = message;
+                    break;
+                case "Error":
+                    ErrorLabel.Text = message;
+                    SuccessLabel.Text = "";
+                    break;
+                default:
+                    ErrorLabel.Text = "";
+                    SuccessLabel.Text = "";
+                    break;
+            }
+        }
+
+        public IQueryable<tblParticipant> GetParticipants([QueryString]string organizationSearchTerm = "",
+                                                      [QueryString]string successLabelMessage = "")
+        {
+            try
+            {
+                RiverWatchEntities _db = new RiverWatchEntities();
+
+                if (!string.IsNullOrEmpty(successLabelMessage))
+                {
+                    SetMessages("Success", successLabelMessage);
+                }
+                int organizationId = 0;
+                if (!string.IsNullOrEmpty(organizationSearchTerm))
+                {
+                    organizationId = _db.organizations
+                                            .Where(o => o.OrganizationName.Equals(organizationSearchTerm))
+                                            .Select(o => o.ID)
+                                            .FirstOrDefault();
+                }
+
+                PropertyInfo isreadonly
+                    = typeof(System.Collections.Specialized.NameValueCollection)
+                            .GetProperty("IsReadOnly", BindingFlags.Instance | BindingFlags.NonPublic);
+                // make collection editable
+                isreadonly.SetValue(this.Request.QueryString, false, null);
+                // remove
+                this.Request.QueryString.Clear();
+
+                var participants = _db.tblParticipants.Where(p => p.OrganizationID == organizationId)
+                                              .OrderBy(p => p.FirstName);
+
+                return participants;
+            }
+            catch (Exception ex)
+            {
+                HandleErrors(ex, ex.Message, "GetParticipants", "", "");
+                return null;
+            }
+        }
+
+        protected void btnSearch_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string organizationSearchTerm = organizationSearch.Text;
+                string redirect = "EditParticipants.aspx?organizationSearchTerm=" + organizationSearchTerm;
+
+                Response.Redirect(redirect, false);
+            }
+            catch (Exception ex)
+            {
+                HandleErrors(ex, ex.Message, "btnSearch_Click", "", "");
+            }
+        }
+
+        protected void btnSearchRefresh_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Response.Redirect("EditParticipants.aspx", false);
+            }
+            catch (Exception ex)
+            {
+                HandleErrors(ex, ex.Message, "btnSearchRefresh_Click", "", "");
+            }
+        }
+
+        [System.Web.Script.Services.ScriptMethod()]
+        [System.Web.Services.WebMethod]
+        public static List<string> SearchForOrganization(string prefixText, int count)
+        {
+            List<string> organizations = new List<string>();
+
+            try
+            {
+                using (RiverWatchEntities _db = new RiverWatchEntities())
+                {
+                    organizations = _db.organizations
+                                        .Where(o => o.OrganizationName.StartsWith(prefixText))
+                                        .Select(c => c.OrganizationName).ToList();
+
+                    return organizations;
+                }
+            }
+            catch (Exception ex)
+            {
+                EditParticipants editParticipants = new EditParticipants();
+                editParticipants.HandleErrors(ex, ex.Message, "SearchForOrganization", "", "");
+                return organizations;
+            }
+        }
+
+        private void HandleErrors(Exception ex, string msg, string fromPage,
+                                                string nam, string comment)
+        {
+            LogError LE = new LogError();
+            LE.logError(msg, fromPage, ex.StackTrace.ToString(), nam, comment);
+
+            SetMessages();
+
+            if (ex.GetType().IsAssignableFrom(typeof(DbEntityValidationException)))
+            {
+                DbEntityValidationException efException = ex as DbEntityValidationException;
+                StringBuilder sb = new StringBuilder();
+
+                foreach (var eve in efException.EntityValidationErrors)
+                {
+                    foreach (var ve in eve.ValidationErrors)
+                    {
+                        sb.AppendFormat("- Property: \"{0}\", Value: \"{1}\", Error: \"{2}\"",
+                            ve.PropertyName,
+                            eve.Entry.CurrentValues.GetValue<object>(ve.PropertyName),
+                            ve.ErrorMessage + Environment.NewLine);
+                    }
+                }
+                SetMessages("Error", sb.ToString());
+            }
+            else
+            {
+                SetMessages("Error", ex.Message);
+            }
+        }
+    }
+}
