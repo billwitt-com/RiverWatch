3dd3010f39e38cf6e4c4346b66b98489f952bb4f
amiscall <amiscall@istonish.com>
2016-11-21 12:44:33 -0700

    Updated model because I Fixed fix the size of the BASIN and SUBBASIN  columns in the tblWBKey table to be varchar(50) so they can hold the basin description’s which are varchar(50 )

---
+++ RWInbound2/Admin/AdminOrgStatus.aspx.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RWInbound2.Admin
+{
+	public partial class AdminOrgStatus
+	{
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
