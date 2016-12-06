519439c83de475ce7207a29ae6f0b21e266c5bd6
amiscall <amiscall@istonish.com>
2016-12-06 16:44:16 -0700

    Almost finished adding new Equipment Editor

--- RWInbound2/Edit/EditEquipment.aspx.designer.cs
+++ RWInbound2/Edit/EditEquipment.aspx.designer.cs
@@ -13,6 +13,15 @@ namespace RWInbound2.Edit {
     public partial class EditEquipment {
         
         /// <summary>
+        /// updatePanelOrgUnknownResults control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.UpdatePanel updatePanelOrgUnknownResults;
+        
+        /// <summary>
         /// ErrorLabel control.
         /// </summary>
         /// <remarks>
@@ -29,15 +38,6 @@ namespace RWInbound2.Edit {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Label SuccessLabel;
-        
-        /// <summary>
-        /// updatePanelOrgUnknownResults control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.UpdatePanel updatePanelOrgUnknownResults;
         
         /// <summary>
         /// orgNameSearch control.
